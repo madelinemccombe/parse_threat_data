372fce4b10af5c8a8c559e5c3e7fd3fe9e0fc397
scottshoaf <sshoaf@paloaltonetworks.com>
2018-07-24 15:35:17 -0400

    added app details from applipedia

--- appcounts.py
+++ appcounts.py
@@ -9,7 +9,6 @@ import json
 from xlrd import open_workbook
 from conf import app_output, app_sheetname, app_source, app_elkindex
 from lib.elk_index import elk_index
-from lib.datatags import filetypetags
 
 
 def appcounts():
@@ -23,27 +22,36 @@ def appcounts():
     filesamples = open_workbook(app_source)
     filesheet = filesamples.sheet_by_name(app_sheetname)
 
-    filetype_data_dict = {}
+    app_data_dict = {}
 
     with open(app_output, 'w') as f:
-        print('flush to rebuild the filetype count file')
+        print('flush to rebuild the application count file')
+
+    with open('lib/apptags.json', 'r') as tagfile:
+        apptags = json.load(tagfile)
 
     for row in range(2, filesheet.nrows):
         timestamp = datetime.datetime(*xlrd.xldate_as_tuple(filesheet.cell(row, 0).value, filesamples.datemode))
-        filetype_data_dict['date'] = str(timestamp.date())
-        filetype_data_dict['application'] = filesheet.cell(row, 1).value
-        filetype_data_dict['count'] = int(filesheet.cell(row,2).value)
+        app_data_dict['date'] = str(timestamp.date())
+        app_data_dict['application'] = filesheet.cell(row, 1).value
+        app_data_dict['count'] = int(filesheet.cell(row,2).value)
 
-# Add in application group if needed
-#        filetype = filetype_data_dict['filetype']
-#        filetype_data_dict['filegroup'] = filetypetags[filetype][0]
-#        filetype_data_dict['afname'] = filetypetags[filetype][1]
+        # Add in application tags from lib/apptags.json
+        # tag with app_unknown if data app not in applipedia
+        appname = app_data_dict['application']
+
+        if appname in apptags:
+            app_data_dict['subcategory'] = apptags[appname]['subcategory']
+            app_data_dict['is-saas'] = apptags[appname]['is-saas']
+        else:
+            app_data_dict['subcategory'] = 'app_unknown'
+            app_data_dict['is-saas'] = 'app_unknown'
 
         index_tag_full = elk_index(app_elkindex)
 
         with open(app_output, 'a') as file:
             file.write(json.dumps(index_tag_full, indent=None, sort_keys=False) + "\n")
-            file.write(json.dumps(filetype_data_dict, indent=None, sort_keys=False) + "\n")
+            file.write(json.dumps(app_data_dict, indent=None, sort_keys=False) + "\n")
 
     return
 
