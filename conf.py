74f169f7515b1857604aecdc442e1097ca40f051
Scott Shoaf <scotchoaf@gmail.com>
2020-05-08 10:50:29 -0400

    rework the code

--- conf.py
+++ conf.py
@@ -1,25 +1,109 @@
-# daily sample file data
-samples_output = 'estackfiles/samplecounts.json'
-samples_source = 'inputfiles/samplecount.xlsx'
-samples_sheetname = 'day'
-malware_source = 'inputfiles/malwaresamplecount.xlsx'
-malware_sheetname = 'Day_mal'
-sample_elkindex = 'samplecounts'
+# common config info
+elastic_url_port = 'localhost:9200'
+input_dir = "inputfiles"
+output_dir = 'metrics_threat_output'
+output_filename = 'metrics_threat_samples'
 
-# filetype sample file data
-filetypes_output = 'estackfiles/filecounts.json'
-filetypes_source = 'inputfiles/malwarefiletype.xlsx'
-filetypes_sheetname = 'malwaretypeday'
-filetype_elkindex = 'samplecounts'
+# start and stop dates
+start_date = '2020-01-01'
+stop_date = '2020-04-30'
 
-# Sig samples with filetype
-sigfiletypes_output = 'estackfiles/sigcounts.json'
-sigfiletypes_source = 'inputfiles/dailyAVsig.xlsx'
-sigfiletypes_sheetname = 'Daily Sig count per filetype'
-sigfiletype_elkindex = 'sigfiletypecounts'
+# metrics_samples.py input filename and worksheet name
+input_file_data = {
+    'verdict_all_all': ['filetypecountall_202001.xlsx', 'Day'],
+    'verdict_all_NO245': ['samplefiletypecountNO245_202001.xlsx', 'Day'],
+    'verdict_mal_all': ['malwarefiletypeall_202001.xlsx', 'Day'],
+    'verdict_mal_NO245': ['malwarefiletypeNO245_202001.xlsx', 'Day'],
+}
 
-# malware samples with application
-app_output = 'estackfiles/appcounts.json'
-app_source = 'inputfiles/malwareapplication.xlsx'
-app_sheetname = 'MalAppDay'
-app_elkindex = 'appcounts'
+# metrics_apps.py input filename and worksheet name
+app_input_file = 'malwareapplication_202001.xlsx'
+app_input_worksheet = 'Day'
+app_output_file = 'metrics_threat_apps'
+
+# file name mappings raw, filegroup, and Autofocus naming
+filetypetags = {
+    "7z": ["7zip", "7zip Archive"],
+    "apk": ["Android", "Android APK"],
+    "bat": ["Script", "batch"],
+    "class": ["Java", "JAVA Class"],
+    "csv": ["CSV", "Comma-Separated Values"],
+    "dex": ["Android", "Android DEX"],
+    "dll": ["DLL", "DLL"],
+    "dll32": ["DLL", "DLL"],
+    "dll64": ["DLL", "DLL64"],
+    "dmg": ["MacOSX", "MacOSX DMG"],
+    "doc": ["Word", "Microsoft Word 97 - 2003 Document"],
+    "docx": ["Word", "Microsoft Word Document"],
+    "elf": ["ELF", "ELF"],
+    "elink": ["Link", "Link"],
+    "exe": ["PE", "PE"],
+    "exe32": ["PE", "PE"],
+    "exe64": ["PE", "PE64"],
+    "fat": ["MacOSX", "Apple's Universal binary file"],
+    "hta": ["HTML", "HTML Application"],
+    "jar": ["Java", "JAVA JAR"],
+    "java": ["Java", "JAVA Class"],
+    "java_class": ["Java", "JAVA Class"],
+    "js": ["Script", "JScript"],
+    "lnk": ["Link", "Link"],
+    "macho": ["MacOSX", "Mach-O"],
+    "macro": ["Macro", " Macro"],
+    "NULL": ["Other", "Other"],
+    "perl": ["Script", "Perl Script"],
+    "pdf": ["PDF", "PDF"],
+    "pkg": ["MacOSX", "Mac OS X app installer"],
+    "ppt": ["Powerpoint", "Microsoft PowerPoint 97 - 2003 Document"],
+    "pptx": ["Powerpoint", "Microsoft PowerPoint Document"],
+    "ps1": ["PowerShell", "PowerShell"],
+    "rar": ["RAR Archive", "RAR Archive"],
+    "rtf": ["RTF", "RTF"],
+    "seven_zip": ["7zip", "7zip Archive"],
+    "shell": ["Shell Script", "Shell Script"],
+    "swf": ["Flash", "Adobe Flash File"],
+    "unknown": ["Other", "Other"],
+    "vbs": ["VBScript", "VBScript"],
+    "xls": ["Excel", "Microsoft Excel 97 - 2003 Document"],
+    "xlsx": ["Excel", "Microsoft Excel Document"],
+    "zbundle": ["MacOSX", " Mac OS X app bundle in ZIP archive"]
+}
+
+sigfiletypetags = {
+    "7z": ["7zip", "7zip Archive"],
+    "APK": ["Android", "Android APK"],
+    "JAVA_CLASS": ["Java", "JAVA Class"],
+    "dll32": ["DLL", "DLL"],
+    "dll64": ["DLL", "DLL64"],
+    "DMG": ["MacOSX", "MacOSX DMG"],
+    "doc": ["Word", "Microsoft Word 97 - 2003 Document"],
+    "docx": ["Word", "Microsoft Word Document"],
+    "ELF": ["ELF", "ELF"],
+    "elink": ["Link", "Link"],
+    "PE": ["PE", "PE"],
+    "exe64": ["PE", "PE64"],
+    "fat": ["MacOSX", "Apple's Universal binary file"],
+    "jar": ["Java", "JAVA JAR"],
+    "java": ["Java", "JAVA Class"],
+    "MACHO": ["MacOSX", "Mach-O"],
+    "macro": ["Macro", " Macro"],
+    "NULL": ["Other", "Other"],
+    "PDF": ["PDF", "PDF"],
+    "PKG": ["MacOSX", "Mac OS X app installer"],
+    "ppt": ["Powerpoint", "Microsoft PowerPoint 97 - 2003 Document"],
+    "pptx": ["Powerpoint", "Microsoft PowerPoint Document"],
+    "RAR": ["RAR Archive", "RAR Archive"],
+    "rtf": ["RTF", "RTF"],
+    "seven_zip": ["7zip", "7zip Archive"],
+    "FLASH": ["Flash", "Adobe Flash File"],
+    "xls": ["Excel", "Microsoft Excel 97 - 2003 Document"],
+    "xlsx": ["Excel", "Microsoft Excel Document"],
+    "zbundle": ["MacOSX", " Mac OS X app bundle in ZIP archive"],
+    "OFFICE": ["Office", "Unknown"],
+    "DNS": ["DNS", "Unknown"],
+    "RAVEN": ["RAVEN", "Unknown"],
+    "OPENOFFICE": ["OPENOFFICE", "Unknown"],
+    "APP": ["APP", "Unknown"],
+    "SWFZWS": ["SWFZWS", "Unknown"],
+    "OLD_PDF": ["OLD_PDF", "Unknown"],
+    "Script": ["Script", "Unknown"]
+}
---
+++ metrics_samples.py
@@ -0,0 +1,156 @@
+####!/usr/bin/env python3
+"""
+Parse data files with json output for estack bulk load
+"""
+
+from datetime import datetime, timedelta
+import sys
+import json
+from xlrd import open_workbook
+import conf
+
+
+def metrics_samples():
+
+    """
+    filetype daily counters for malware verdict samples
+
+    """
+
+    # create list of filetypes to check for new additions
+    filetype_list = []
+
+    # grab all input data and write into a master dict
+    raw_data = {}
+    
+    for datatype in conf.input_file_data:
+        input_file = open_workbook(f'{conf.input_dir}/{conf.input_file_data[datatype][0]}')
+        input_sheet = input_file.sheet_by_name(conf.input_file_data[datatype][1])
+        raw_data[datatype] = {}
+
+        for row in range(2, input_sheet.nrows):
+            row_date = input_sheet.cell(row, 0).value
+            file_type = input_sheet.cell(row, 1).value
+            file_count = int(input_sheet.cell(row, 2).value)
+
+            if row_date in raw_data[datatype]:
+                pass
+            else:
+                raw_data[datatype][row_date] = {}
+
+            raw_data[datatype][row_date][file_type] = file_count
+
+            # generate list of all filetypes
+            if file_type in filetype_list:
+                pass
+            else:
+                filetype_list.append(input_sheet.cell(row, 1).value)
+
+    # check that all filetypes are captured
+    # if any are missing stop and user has to update the file list in conf.py
+    missing_filetypes = []
+    for filetype in filetype_list:
+        if filetype in conf.filetypetags:
+            pass
+        else:
+            missing_filetypes.append(filetype)
+
+    if len(missing_filetypes) > 0:
+        print('found missing filetypes')
+        print(missing_filetypes)
+        sys.exit()
+
+    # timestamp to be added to the file name
+    filedate = datetime.now().strftime('%Y-%m-%dT%H-%M-%SZ')
+
+    # create empty json output file
+    with open(f'{conf.output_dir}/{conf.output_filename}-{filedate}.json', 'w') as f:
+        print(f'writing to file: {conf.output_dir}/{conf.output_filename}-{filedate}.json')
+
+    # set start date for appending the data set
+    start_date = datetime.strptime(conf.start_date, '%Y-%m-%d')
+    stop_date = datetime.strptime(conf.stop_date, '%Y-%m-%d')
+    item_date = start_date
+
+    # generate daily stats and write to dict for csv output
+    while item_date <= stop_date:
+
+        # first formats the date for elasticsearch and second to search input values
+        raw_date = item_date.strftime('%Y-%m-%d')
+
+        for filetype in filetype_list:
+            stats_dict = {}
+            stats_dict['date'] = item_date.strftime('%Y-%m-%dT00:00:00Z')
+            stats_dict['metrics.threat.samples.filetype.raw'] = filetype
+            stats_dict['metrics.threat.samples.filetype.group'] = conf.filetypetags[filetype][0]
+            stats_dict['metrics.threat.samples.filetype.autofocus'] = conf.filetypetags[filetype][1]
+
+            # check raw_dict and add counts if exist or set to zero
+            # get counts for all verdicts and all sources
+            if filetype in raw_data['verdict_all_all'][raw_date]:
+                stats_dict['metrics.threat.samples.verdict.all.all'] = raw_data['verdict_all_all'][raw_date][filetype]
+            else:
+                stats_dict['metrics.threat.samples.verdict.all.all'] = 0
+                
+            # get counts for all verdicts and customer source
+            if filetype in raw_data['verdict_all_NO245'][raw_date]:
+                stats_dict['metrics.threat.samples.verdict.all.customer'] = raw_data['verdict_all_NO245'][raw_date][filetype]
+            else:
+                stats_dict['metrics.threat.samples.verdict.all.customer'] = 0
+
+            # get counts for all verdicts and all sources
+            if filetype in raw_data['verdict_mal_all'][raw_date]:
+                stats_dict['metrics.threat.samples.verdict.malware.all'] = raw_data['verdict_mal_all'][raw_date][filetype]
+            else:
+                stats_dict['metrics.threat.samples.verdict.malware.all'] = 0
+
+            # get counts for all verdicts and customer source
+            if filetype in raw_data['verdict_mal_NO245'][raw_date]:
+                stats_dict['metrics.threat.samples.verdict.malware.customer'] = raw_data['verdict_mal_NO245'][raw_date][filetype]
+            else:
+                stats_dict['metrics.threat.samples.verdict.malware.customer'] = 0
+
+            # calculate toxicity ratio comparing malware vs all verdicts - customer source
+            if filetype in raw_data['verdict_all_NO245'][raw_date] and filetype in raw_data['verdict_mal_NO245'][raw_date]:
+                stats_dict['metrics.threat.samples.toxicity.customer'] = raw_data['verdict_mal_NO245'][raw_date][filetype] / \
+                                                                         raw_data['verdict_all_NO245'][raw_date][filetype]
+
+            # calculate toxicity ratio comparing malware vs all verdicts - all sources
+            if filetype in raw_data['verdict_all_all'][raw_date] and filetype in raw_data['verdict_mal_all'][raw_date]:
+                stats_dict['metrics.threat.samples.toxicity.all'] = raw_data['verdict_mal_all'][raw_date][filetype] / \
+                                                                    raw_data['verdict_all_all'][raw_date][filetype]
+
+            # calculate feed counts for all verdicts as diff of all and customer sources
+            stats_dict['metrics.threat.samples.verdict.all.feed'] = \
+                stats_dict['metrics.threat.samples.verdict.all.all'] - \
+                stats_dict['metrics.threat.samples.verdict.all.customer']
+
+            # calculate feed counts for malware verdicts as diff of all and customer sources
+            stats_dict['metrics.threat.samples.verdict.malware.feed'] = \
+                stats_dict['metrics.threat.samples.verdict.malware.all'] - \
+                stats_dict['metrics.threat.samples.verdict.malware.customer']
+
+            # create elk index by month and year
+            index_tag_full = {}
+            index_tag_inner = {}
+            elk_index_name = f"metrics-threat-samples-{item_date.year}-{item_date.strftime('%m')}"
+            index_tag_inner['_index'] = f'{elk_index_name}'
+            index_tag_inner['_type'] = f'{elk_index_name}'
+            index_tag_full['index'] = index_tag_inner
+
+            with open(f'{conf.output_dir}/{conf.output_filename}-{filedate}.json', 'a') as file:
+                file.write(json.dumps(index_tag_full, indent=None, sort_keys=False) + "\n")
+                file.write(json.dumps(stats_dict, indent=None, sort_keys=False) + "\n")
+
+        item_date = item_date + timedelta(days=1)
+
+    # print out the elasticSearch bulk load curl commands
+    print('\nUse curl -XDELETE [url]:[port]/index to delete data from the index')
+    print('use the XPOST curl command to load json data to elasticSearch')
+    print('add -u with username:password if security features enabled\n')
+
+    print(
+        f'curl -s -XPOST \'http://{conf.elastic_url_port}/_bulk\' --data-binary @{conf.output_dir}/{conf.output_filename}-{filedate}.json -H \"Content-Type: application/x-ndjson\" \n')
+
+if __name__ == '__main__':
+    metrics_samples()
